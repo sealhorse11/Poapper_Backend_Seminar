ab49423ae81ff7c6236599c64a9aeac64168fcbe
narco lepsy <57515842+sealhorse11@users.noreply.github.com>
2022-05-02 23:35:39 +0900

    Create prob1.js

---
+++ week3/prob1.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+const moment = require('moment');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    if(req.url == '/'){
+        res.write('<h1></h1>')
+        res.end()
+    }
+    if(req.url == '/timer'){
+        const now = moment().format('YYYY-MM-DD HH:MM:SS');
+        res.write('<h1>' + now +'</h1>')
+        res.end()
+    }
+}).listen(8080);
+
+server.on('listening', () => {
+    console.log('server is running on 8080');
+})
+
+server.on('error', (error) => {
+    if(error) console.log(error)
+})
